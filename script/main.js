77c37adef6e70ab06c4a0a028a0e9674fc89abaa
rubenrbnc <rubenmelchers1994@gmail.com>
2016-04-25 18:56:22 +0200

    profile picture hover effect
    added an onhover effect on the profile picture in the about me section.

--- script/main.js
+++ script/main.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
     var jssor_slider1 = new $JssorSlider$("slider1_container", options);
 
 });
+
+var min = -100;
+var max = 100;
+
+$('polygon').hover(function () {
+    $(this).css({"fill-opacity": "0", "transform":"translate3d("+ Math.floor(Math.random()*(max-min+1)+min) +"px, "+ Math.floor(Math.random()*(max-min+1)+min) +"px, "+ Math.floor(Math.random()*(max-min+1)+min) +"px)"});
+    // $('.img-above-svg').css({"opacity": "0", "z-index":"-1"});
+    $(this).on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+        function() {
+            $(this).css({"fill-opacity": "1", "transform":"translate3d(0, 0, 0)"});
+            // $('.img-above-svg').css({"opacity": "1", "z-index":"-1"});
+        });
+
+});
